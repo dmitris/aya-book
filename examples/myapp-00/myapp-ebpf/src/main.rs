9cf1784e80c6314ef5bafc16de36ab718b89721c
Dmitry S <dsavints@gmail.com>
2022-01-07 13:21:27 +0100

    sync the xdp example with generated code

--- examples/myapp-00/myapp-ebpf/src/main.rs
+++ examples/myapp-00/myapp-ebpf/src/main.rs
@@ -1,7 +1,29 @@
 #![no_std]
 #![no_main]
 
+// ANCHOR: use
+use aya_bpf::{
+    bindings::xdp_action,
+    macros::xdp,
+    programs::XdpContext,
+};
+// ANCHOR_END: use
+
+// ANCHOR: main
+#[xdp(name="myapp")]
+pub fn myapp(ctx: XdpContext) -> u32 {
+    match unsafe { try_myapp(ctx) } {
+        Ok(ret) => ret,
+        Err(_) => xdp_action::XDP_ABORTED,
+    }
+}
+
+unsafe fn try_myapp(_ctx: XdpContext) -> Result<u32, u32> {
+    Ok(xdp_action::XDP_PASS)
+}
+// ANCHOR_END: main
+
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
-    unreachable!()
+    unsafe { core::hint::unreachable_unchecked() }
 }
